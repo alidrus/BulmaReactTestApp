2ce771682db8fa427400da8daff287d738e5b769
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2024-02-19 12:54:15 +0800

    Allow to choose which component to load based on menu item clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import Food from './components/Food';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import StopWatch from './components/StopWatch';
 
 function App() {
+  const [menuItem, setMenuItem] = useState('food-list');
+
+  const clickHandler = (item) => {
+    setMenuItem(item);
+  };
+
   return (
     <>
-      <Header></Header>
-      <Food></Food>
+      <Header clickHandler={clickHandler}></Header>
+      {menuItem === 'food-list' && <Food></Food>}
+      {menuItem === 'stopwatch' && <StopWatch></StopWatch>}
       <Footer></Footer>
     </>
   );
