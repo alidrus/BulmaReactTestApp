376d34e202f479c7d300670eaa20b4af399f5642
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2024-02-19 14:02:39 +0800

    Added formatElapsedTime() function exported as formatElapsedTime.

--- src/utilities.js
+++ src/utilities.js
@@ -2,6 +2,16 @@ const padZero = (number) => {
     return number.toString(10).padStart(2, '0');
 };
 
+const formatElapsedTime = (elapsedTime) => {
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60)),
+        minutes = Math.floor(elapsedTime / (1000 * 60) % 60),
+        seconds = Math.floor(elapsedTime / 1000 % 60),
+        milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}:${padZero(milliseconds)}`;
+};
+
 export {
     padZero,
+    formatElapsedTime,
 };
